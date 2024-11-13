1f48117b879e919396be09ecd7ad5a13f8f898d1
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-11-13 05:01:22 +0000

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 """
-This module defines a flask web app
+This module creates the Flask app and registers the blueprints
 """
 
-from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
 
@@ -17,7 +16,7 @@ CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """This method handles the teardown, on itself"""
+    """Closes the storage on teardown"""
     storage.close()
 
 
@@ -29,5 +28,5 @@ def error_404(exception):
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", 5000)
+    port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
